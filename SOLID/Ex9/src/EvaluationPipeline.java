4e89ac1e95d6cfbf8537461f33a6d1bdec8fd147
SammyUrfen <bibekcharah@gmail.com>
2026-03-07 23:26:01 +0530

    ex9

--- SOLID/Ex9/src/EvaluationPipeline.java
+++ SOLID/Ex9/src/EvaluationPipeline.java
@@ -1,11 +1,18 @@
 public class EvaluationPipeline {
+
+    private final Rubric rubric;
+    private final PlagiarismCheck pc;
+    private final Grader grader;
+    private final ReportOutput writer;
+
+    public EvaluationPipeline(PlagiarismCheck pc, Grader grader, ReportOutput writer, Rubric rubric) {
+        this.rubric = rubric;
+        this.pc = pc;
+        this.grader = grader;
+        this.writer = writer;
+    }
     // DIP violation: high-level module constructs concretes directly
     public void evaluate(Submission sub) {
-        Rubric rubric = new Rubric();
-        PlagiarismChecker pc = new PlagiarismChecker();
-        CodeGrader grader = new CodeGrader();
-        ReportWriter writer = new ReportWriter();
-
         int plag = pc.check(sub);
         System.out.println("PlagiarismScore=" + plag);
 
--- SOLID/Ex9/src/Main.java
+++ SOLID/Ex9/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Evaluation Pipeline ===");
         Submission sub = new Submission("23BCS1007", "public class A{}", "A.java");
-        new EvaluationPipeline().evaluate(sub);
+        PlagiarismCheck pc = new PlagiarismChecker();
+        Grader grader = new CodeGrader();
+        ReportOutput writer = new ReportWriter();
+        Rubric rubric = new Rubric();
+
+        new EvaluationPipeline(pc, grader, writer, rubric).evaluate(sub);
     }
 }
